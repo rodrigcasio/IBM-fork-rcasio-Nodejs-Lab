086fc585f9d8760cfeff4edbf183371dfb054529
rodrigcasio <rod.casio@outlook.com>
2025-09-28 15:25:40 -0600

    Added a commented section on how to use arrow functions for differnet away to initiate a server

--- CD220Labs/http_server/index.js
+++ CD220Labs/http_server/index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Define the request listener function
-const requestListener = function (req, res) {
+const requestListener = (req, res) => {
     res.writeHead(200); // Set the status code to 200 (OK)
     res.end('Hello, World!'); // Send the response "Hello, World!"
 };
@@ -16,3 +16,23 @@ const server = http.createServer(requestListener);
 // Start the server and listen on the specified port
 server.listen(port);
 console.log('Server listening on port: ' + port);
+
+/*
+    Another way to do what is done above
+
+    const http = require('http');
+    const port = 8080;
+    const hostname = 'localhost'
+    
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain'):
+
+        res.end('Hello, World!');
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${host}:${port}/`);
+    })
+
+*/
