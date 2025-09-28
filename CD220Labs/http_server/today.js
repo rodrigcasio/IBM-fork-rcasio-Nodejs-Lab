186cc9833ef4f9e82590b101a71d2b95bf50ce9d
rodrigcasio <rod.casio@outlook.com>
2025-09-28 16:58:13 -0600

    Modified default code from the module and set it up with an arrow function

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,21 @@
- // Export a function named 'getDate' from the module
+// modified by rcasio using the arrow function 
+
+// 1. Export a function named "getDate" from the module
+// 2. inside the function, get the current date and time in the timezone "Australia/Brisbane"
+// 3. lastly, return the formated date and time
+
+module.exports.getDate = () => {
+    let australiaEstTime = new Date().toLocaleDateString("en-US", {timeZone: "Australia/Brisbane"});
+    return australiaEstTime;
+}
+
+/* 
+    DEFAULT CODE ADDED BY IBM 
+
+// Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
     // Get the current date and time in the timezone "Australia/Brisbane"
     let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
     return aestTime; // Return the formatted date and time
 };
+ */
