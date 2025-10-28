23e13488f47d6e9485dd559f107f0b0c6fca30f3
rodrigcasio <rod.casio@outlook.com>
2025-10-28 17:17:35 -0600

    Added new endpoint `/fetchMonth/:num` within the `expressServer.js` server app

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -7,6 +7,8 @@ const app = new express();
 // Initialize an array to store login details
 let loginDetails = [];
 
+app.use(express.json());
+
 // Define the root route to send a welcome message
 app.get("/", (req, res) => {
     res.send("Welcome to the express server");
@@ -27,6 +29,32 @@ app.post("/login/:name", (req, res) => {
 app.get("/:name", (req, res) => {
     res.send("Hello " + req.params.name);
 });
+
+// new endpoint
+app.get('/fetchMonth/:num', (req, res) => {
+    const num = req.params.num;
+
+    if (num === null || num <= 0) {
+      return send.status(400).json({ message: 'Invalid number' });
+   } 
+   
+   switch (num) {
+     case '1': res.status(200).json({ message: 'January' }); break;
+     case '2': res.status(200).json({ message: 'Febrary' }); break;
+     case '3': res.status(200).json({ message: 'March' }); break;
+     case '4': res.status(200).json({ message: 'April' }); break;
+     case '5': res.status(200).json({ message: 'May' }); break;
+     case '6': res.status(200).json({ message: 'June' }); break;
+     case '7': res.status(200).json({ message: 'July' }); break;
+     case '8': res.status(200).json({ message: 'August' }); break;
+     case '9': res.status(200).json({ message: 'September' }); break;
+     case '10': res.status(200).json({ message: 'October' }); break;
+     case '11': res.status(200).json({ message: 'November' }); break;
+     case '12': res.status(200).json({ message: 'December' }); break;
+     default: res.status(400).json({ message: 'Invalid month number' });
+   }
+
+ });
 
 // Start the server and listen on port 3333
 app.listen(3333, () => {
